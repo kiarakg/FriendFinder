42a3bcf4642b07cbdb8b781af243267a6aa438fe
Kiara Guzman <kiara.kg11@gmail.com>
2019-09-25 02:09:39 -0400

    apiRoutes fixed

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,63 +1,55 @@
 var friends = require("../data/friends.js");
 
 module.exports = function(app) {
+    // api path to get the friends data
+    // responds with a jspn object (an array of friends)
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
 
     app.post("/api/friends", function(req, res) {
-        var totalDifference = 0;
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
-        var userData = req.body;
-        var userName = userData.name;
-        var userScores = userData.scores;
+        // newFriend is the user filling out the survey
+        var newFriend = req.body;
 
-        var b = userScores.map(function(item) {
-            return parseInt(item, 10);
-        });
+        var bestMatch = {};
 
-        userData = {
-            name: req.body.name,
-            photo: req.body.photo,
-            scores: b
-        };
-
-        console.log("Nmae: " + userName);
-        console.log("User score " + userScores);
-
-        var sum = b.reduce((a, b) => a + b, 0);
-        console.log("Sum of users score " + sum);
-        console.log("Best match friend diff " + bestMatch.friendDifference);
-        console.log("");
-
-        for(var i = 0; i < friends.length; i++) {
-            console.log(friends[i].name);
-            totalDifference = 0;
-            console.log("Total Difference " + totalDifference);
-            console.log("Best match friend diff " + bestMatch.friendDifference);
-
-            var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
-            console.log("Total friend score " + bfriendScore);
-            totalDifference += Math.abs(sum - bfriendScore);
-            console.log("---------->" + totalDifference); 
-
-            if(totalDifference <= bestMatch.friendDifference) {
-                bestMatch.name = friends[i].name;
-                bestMatch.photo = friends[i].photo;
-                bestMatch.friendDifference = totalDifference;
+        for(var i = 0; i < newFriend.scores.length; i++) {
+            if(newFriend.score[i] == "1 (Strongly Disagree)") {
+                newFriend.scores[i] = 1;
+            } else if(newFriend.score[i] == "5 (Strongly Agree)") {
+                newFriend.scores[i] = 5;
+            } else {
+                newFriend.scores[i] = parseInt(newFriend.scores[i]);
             }
-            console.log(totalDifference + "Total Difference");
         }
 
-        console.log(bestMatch);
-        friends.push(userData);
-        console.log("New User added");
-        console.log(userData);
-        res.json(bestMatch);
+        var bestMatchIndex = 0;
+        var bestMatchDiff = 40;
 
-    });
-};
+        for(var i = 0; i < friends.length; i++) {
+            var totalDifference = 0;
+
+            for(var index = 0; index < friends[i].scores.length; i++) {
+                var differenceOneScore = Math.abs(friends[i].scores[index] - newFriend.scores[index]);
+                totalDifference += differenceOneScore;
+            }
+
+            if(totalDifference < bestMatchDiff) {
+                bestMatchIndex = i;
+                bestMatchDiff = totalDifference;
+            }
+        }
+
+        // The bestMatchIndex is used to get the bestMatch data from friends index
+        bestMatch = friends[bestMatchIndex];
+
+        // Put new friend from survey in database array
+        friends.push(newFriend);
+
+        // Return the best match friend
+        res.json(bestMatch);
+            
+        });
+
+    };
+       
