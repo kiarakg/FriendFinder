29bfc049daf4ade9146f3de326fe34e212c32e20
Kiara Guzman <kiara.kg11@gmail.com>
2019-09-25 02:19:58 -0400

    htmlRoutes updated

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,21 @@
+// Dependencies
 var path = require("path");
 
+// Routing
 module.exports = function(app) {
-    app.get("/survey", function(req, res){
-        res.sendFile(path.join(__dirname + "/../public/survey.html"));
+    // HTML GET Requests
+    // Handles when users visit a page
+    // User is shown an HTML page of content
+
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
-    app.use(function(req, res){
-        res.sendFile(path.join(__dirname + "/../public/home.html"));
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-}
+
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
