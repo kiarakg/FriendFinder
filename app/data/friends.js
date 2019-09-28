86d5996769c9d3b60b57a088ddd50d9c23620422
Kiara Guzman <kiara.kg11@gmail.com>
2019-09-28 16:19:00 -0400

    friends list updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,32 +2,32 @@ var friends = [
     {
         name: "Snoop Dogg",
         photo: "https://www.biography.com/.image/t_share/MTQ3NjM5ODIyNjU0MTIxMDM0/snoop_dogg_photo_by_estevan_oriol_archive_photos_getty_455616412.jpg",
-        scores: [1,2,3,4,5,1,2,3,4,5]
+        scores: [1,4,3,4,5,2,5,5,4,4]
     },
     {
         name: "Bill Gates",
         photo: "https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X_400x400.jpg",
-        scores: [1,2,3,4,5,1,2,3,4,4]
+        scores: [1,3,2,2,4,5,5,5,2,3]
     },
     {
         name: "Oprah",
         photo: "https://sites.google.com/site/whoamibyhiralborania/_/rsrc/1483271313707/global-entrepreneurs/oprah-winfrey/Oprah%20Winfrey.jpg?height=400&width=320",
-        scores: [1,2,3,4,5,1,2,3,4,3]
+        scores: [3,3,4,3,5,5,3,5,5,4]
     },
     {
         name: "Voldemort",
         photo: "https://qph.fs.quoracdn.net/main-qimg-140bb8e3d8c9980fec152ee65c2cb16b",
-        scores: [1,2,3,4,5,1,2,3,4,2]
+        scores: [1,1,1,3,1,5,1,1,1,1]
     },
     {
         name: "Jon Snow",
         photo: "https://pbs.twimg.com/profile_images/901947348699545601/hqRMHITj_400x400.jpg",
-        scores: [1,2,3,4,5,1,2,3,4,1]
+        scores: [5,2,5,4,5,5,5,5,2,1]
     },
     {
         name: "Tyrone Biggums",
         photo: "https://i.imgflip.com/2clrac.jpg",
-        scores: [4,4,2,3,2,2,3,2,5,4]
+        scores: [2,3,5,5,5,5,1,1,2,2]
     }
 
 ];
